415d28d533c08048be8d3fd566430ec880e9572e
Quang Dat <qdatqb@gmail.com>
2022-10-29 18:48:46 +0700

    add enviroment file

--- src/layout/MainLayout/Sidebar/MenuList/CustomIcon/index.js
+++ src/layout/MainLayout/Sidebar/MenuList/CustomIcon/index.js
@@ -6,9 +6,12 @@ const CustomIcon = ({ type, name, svg, className }) => {
   const IconComponent = Icons[name];
 
   if (type === 'BASE64' && svg) {
-    return <SvgIcon className={className}>
-      <g dangerouslySetInnerHTML={{ __html: svg }} />
-    </SvgIcon>;
+    return (
+      <SvgIcon className={className}>
+        <g dangerouslySetInnerHTML={{ __html: svg }} />
+      </SvgIcon>
+    );
+    // return <img src={svg} alt={name} className={className}/>;
   }
 
   if (!name || !IconComponent) {
