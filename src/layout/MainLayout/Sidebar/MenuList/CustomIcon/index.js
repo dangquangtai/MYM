9d2102802bec0c8cfcd3bebe7774dca52305934d
Quang Dat <qdatqb@gmail.com>
2022-11-08 19:19:52 +0700

    adjust icon menu item

--- src/layout/MainLayout/Sidebar/MenuList/CustomIcon/index.js
+++ src/layout/MainLayout/Sidebar/MenuList/CustomIcon/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import * as Icons from '@material-ui/icons';
 import SvgIcon from '@material-ui/core/SvgIcon';
+import useStyles from './../../../../../utils/classes';
 
-const CustomIcon = ({ type, name, svg, className }) => {
+const CustomIcon = ({ type, name, svg, base64, className }) => {
+  const classes = useStyles();
   const IconComponent = Icons[name];
 
-  if (type === 'BASE64' && svg) {
+  if (type === 'SVG' && svg) {
     return (
       <SvgIcon className={className}>
         <g dangerouslySetInnerHTML={{ __html: svg }} />
       </SvgIcon>
     );
-    // return <img src={svg} alt={name} className={className}/>;
+  }
+
+  if (type === 'BASE64' && base64) {
+    return <img src={base64} alt={name} className={classes.iconBase64} />;
   }
 
   if (!name || !IconComponent) {
